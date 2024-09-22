fe98f3805f85bda27c88dab17329f719a237503a
Bayu Tantra Bramandhita <bayu.tantra@dansmultipro.com>
2024-09-22 13:06:33 +0700

    remove unused toast

--- feature/chat/src/main/java/com/bayutb/chat/presentation/fragment/ChatListFragment.kt
+++ feature/chat/src/main/java/com/bayutb/chat/presentation/fragment/ChatListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -53,9 +52,6 @@ class ChatListFragment : Fragment(), ChatListAdapter.OnClickListener {
     }
 
     override fun onClick(chat: Chat) {
-        Toast.makeText(context, chat.sender, Toast.LENGTH_SHORT).also {
-            it.show()
-        }
         (activity as ChatListActivity).goToDetailFragment(chat.id)
     }
 }
