3fadaf9b3c6053a7f43327fd314f33d96a35bb55
Bayu Tantra Bramandhita <bayu.tantra@dansmultipro.com>
2024-09-22 12:57:49 +0700

    remove unused code

--- feature/chat/src/main/java/com/bayutb/chat/di/module/ChatModule.kt
+++ feature/chat/src/main/java/com/bayutb/chat/di/module/ChatModule.kt
@@ -9,11 +9,6 @@ import retrofit2.Retrofit
 @Module
 object ChatModule {
     @Provides
-    fun provideGreetingString() : String {
-        return "Hello ges!"
-    }
-
-    @Provides
     fun provideChatRepository(retrofit: Retrofit) : ChatRepository {
         return ChatDataSource(retrofit)
     }
