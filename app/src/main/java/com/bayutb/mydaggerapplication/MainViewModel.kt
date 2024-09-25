c134aeb108a452a2ce433e0509d9d0b0c2788e2c
Bayu Tantra Bramandhita <bayu.tantra@dansmultipro.com>
2024-09-25 09:38:00 +0700

    enhance MainViewModel

--- app/src/main/java/com/bayutb/mydaggerapplication/MainViewModel.kt
+++ app/src/main/java/com/bayutb/mydaggerapplication/MainViewModel.kt
@@ -3,15 +3,16 @@ package com.bayutb.mydaggerapplication
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.distinctUntilChanged
 import com.bayutb.core.repository.DataStoreRepository
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 class MainViewModel(
     private val dataStoreRepository: DataStoreRepository
 ): ViewModel() {
-    val user = dataStoreRepository.getUser().asLiveData().distinctUntilChanged()
+    val user = dataStoreRepository.getUser().distinctUntilChanged().asLiveData()
 }
 
+@Suppress("UNCHECKED_CAST")
 class MainViewModelProvider(
     private val dataStoreRepository: DataStoreRepository
 ): ViewModelProvider.Factory {
@@ -19,6 +20,6 @@ class MainViewModelProvider(
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(dataStoreRepository) as T
         }
-        return super.create(modelClass)
+        throw IllegalArgumentException("Check MainViewModelProvider!!")
     }
 }
